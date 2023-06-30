397f6de8342b1aa715213f784f2f0068ea49d677
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-06-30 19:07:24 +0200

    [test] [refactor] Share common proc in test_secure_proc

--- tests/test_secure_proc.py
+++ tests/test_secure_proc.py
@@ -4,6 +4,7 @@ import os
 import pytest
 import shutil
 import unittest
+from contextlib import contextmanager
 
 from benchmark_simulator._constants import (
     _SharedDataFileNames,
@@ -48,6 +49,13 @@ def test_get_timeout_message():
     assert isinstance(_get_timeout_message("dummy1", "dummy2/dummy3.json"), str)
 
 
+@contextmanager
+def common_proc() -> None:
+    _init_for_tests()
+    yield None
+    shutil.rmtree(DIR_NAME)
+
+
 def test_init_simulator():
     _init_for_tests()
     for fn in _get_file_paths(DIR_NAME).__dict__.values():
@@ -62,79 +70,66 @@ def test_init_simulator_existing():
     with open(os.path.join(DIR_NAME, _SharedDataFileNames.result.value), mode="w"):
         pass
 
-    _init_for_tests()
-    for fn in _SharedDataFileNames:
-        assert json.load(open(os.path.join(DIR_NAME, fn.value))) == {}
-
-    shutil.rmtree(DIR_NAME)
+    with common_proc() as _:
+        for fn in _SharedDataFileNames:
+            assert json.load(open(os.path.join(DIR_NAME, fn.value))) == {}
 
 
 def test_allocate_proc_to_worker():
-    _init_for_tests()
-    path = os.path.join(DIR_NAME, _SharedDataFileNames.proc_alloc.value)
-    ans = {}
-    for i in range(10):
-        _allocate_proc_to_worker(path, pid=i * 100, lock=LOCK)
-        assert _is_allocation_ready(path, n_workers=i + 1, lock=LOCK)
-        ans[i * 100] = i
+    with common_proc() as _:
+        path = os.path.join(DIR_NAME, _SharedDataFileNames.proc_alloc.value)
+        ans = {}
+        for i in range(10):
+            _allocate_proc_to_worker(path, pid=i * 100, lock=LOCK)
+            assert _is_allocation_ready(path, n_workers=i + 1, lock=LOCK)
+            ans[i * 100] = i
 
-    assert _complete_proc_allocation(path, lock=LOCK) == ans
-    assert _get_worker_id_to_idx(path, lock=LOCK) == {str(p): i for i, p in enumerate(ans.keys())}
+        assert _complete_proc_allocation(path, lock=LOCK) == ans
+        assert _get_worker_id_to_idx(path, lock=LOCK) == {str(p): i for i, p in enumerate(ans.keys())}
 
-    _wait_proc_allocation(path, n_workers=10, lock=LOCK)
-    with pytest.raises(TimeoutError):
-        _wait_proc_allocation(path, n_workers=11, time_limit=0.1, lock=LOCK)
-
-    shutil.rmtree(DIR_NAME)
+        _wait_proc_allocation(path, n_workers=10, lock=LOCK)
+        with pytest.raises(TimeoutError):
+            _wait_proc_allocation(path, n_workers=11, time_limit=0.1, lock=LOCK)
 
 
 def test_record_cumtime():
-    _init_for_tests()
-    path = os.path.join(DIR_NAME, _SharedDataFileNames.worker_cumtime.value)
-
-    ans = {}
-    worker_ids = "abcdefghij"
-    min_id, min_cumtime = "*", 1000
-    n_reg = 0
-    for j in range(2):
-        for i in range(10):
+    with common_proc() as _:
+        path = os.path.join(DIR_NAME, _SharedDataFileNames.worker_cumtime.value)
+        ans = {}
+        worker_ids = "abcdefghij"
+        min_id, min_cumtime = "*", 1000
+        n_reg = 0
+        for k in range(20):
+            i, j = k % 10, k // 10
             worker_id = worker_ids[i]
             cumtime = i + 0.3 * j
             _record_cumtime(path, worker_id=worker_id, cumtime=cumtime, lock=LOCK)
             ans[worker_id] = cumtime
             if cumtime < min_cumtime:
-                min_cumtime = cumtime
-                min_id = i
+                min_cumtime, min_id = cumtime, i
 
-            assert ans == _fetch_cumtimes(path, lock=LOCK)
             n_reg = max(i + 1, n_reg)
+            assert ans == _fetch_cumtimes(path, lock=LOCK)
             assert _is_simulator_ready(path, n_workers=n_reg, lock=LOCK)
-            itr = range(10) if j == 1 else range(i + 1)
-            for idx in itr:
+            for idx in (range(10) if j == 1 else range(i + 1)):
                 worker_id = worker_ids[idx]
-                c1 = bool(_is_min_cumtime(path, worker_id=worker_id, lock=LOCK))
-                c2 = bool(min_id == idx)
-                assert not (c1 ^ c2)
-                if c1:
+                is_min_cumtime = bool(_is_min_cumtime(path, worker_id=worker_id, lock=LOCK))
+                assert not (is_min_cumtime ^ bool(min_id == idx))
+                if is_min_cumtime:
                     _wait_until_next(path, worker_id=worker_id, lock=LOCK, waiting_time=1e-4)
-                else:
-                    pass
 
-    _wait_all_workers(path, n_workers=n_reg, lock=LOCK)
-    with pytest.raises(TimeoutError):
-        _wait_all_workers(path, n_workers=n_reg + 1, time_limit=0.1, lock=LOCK)
-
-    shutil.rmtree(DIR_NAME)
+        _wait_all_workers(path, n_workers=n_reg, lock=LOCK)
+        with pytest.raises(TimeoutError):
+            _wait_all_workers(path, n_workers=n_reg + 1, time_limit=0.1, lock=LOCK)
 
 
 def test_cache_state():
-    # _StateType = Tuple[_RuntimeType, _CumtimeType, _FidelityType, _SeedType]
-    _init_for_tests()
-    path = os.path.join(DIR_NAME, _SharedDataFileNames.state_cache.value)
-    cumtime = 0.0
-    ans = []
-    for update in [False, True]:
-        for i in range(10):
+    with common_proc() as _:
+        path = os.path.join(DIR_NAME, _SharedDataFileNames.state_cache.value)
+        cumtime = 0.0
+        ans = []
+        for k in range(20):
+            i, update = k % 10, bool(k // 10)
             cumtime += i
             state = _StateType(runtime=float(i), cumtime=cumtime, fidel=i, seed=i)
             if update:
@@ -144,38 +139,33 @@ def test_cache_state():
                 _cache_state(path, config_hash=0, new_state=state, lock=LOCK)
                 ans.append(state)
 
-            print(ans, _fetch_cache_states(path, config_hash=0, lock=LOCK))
             assert _fetch_cache_states(path, config_hash=0, lock=LOCK) == ans
 
-    for idx in [5, 6, 7, 4, 3, 2, 1, 0, 0]:
-        _delete_state(path, config_hash=0, index=idx, lock=LOCK)
-        ans.pop(idx)
-        assert _fetch_cache_states(path, config_hash=0, lock=LOCK) == ans
-    else:
-        _delete_state(path, config_hash=0, index=idx, lock=LOCK)
-        assert _fetch_cache_states(path, config_hash=0, lock=LOCK) == []
-
-    shutil.rmtree(DIR_NAME)
+        for idx in [5, 6, 7, 4, 3, 2, 1, 0, 0]:
+            _delete_state(path, config_hash=0, index=idx, lock=LOCK)
+            ans.pop(idx)
+            assert _fetch_cache_states(path, config_hash=0, lock=LOCK) == ans
+        else:
+            _delete_state(path, config_hash=0, index=idx, lock=LOCK)
+            assert _fetch_cache_states(path, config_hash=0, lock=LOCK) == []
 
 
 def test_record_result():
-    _init_for_tests()
-    path = os.path.join(DIR_NAME, _SharedDataFileNames.result.value)
-    ans = {"cumtime": [], "loss": []}
-    for i in range(19):
-        _record_result(path, results={"loss": i, "cumtime": i}, lock=LOCK)
-        ans["loss"].append(i)
-        ans["cumtime"].append(i)
-        assert not _is_simulator_terminated(path, max_evals=20, lock=LOCK)
-        assert json.load(open(path)) == ans
-    else:
-        _record_result(path, results={"loss": i + 1, "cumtime": i + 1}, lock=LOCK)
-        ans["loss"].append(i + 1)
-        ans["cumtime"].append(i + 1)
-        assert _is_simulator_terminated(path, max_evals=20, lock=LOCK)
-        assert json.load(open(path)) == ans
-
-    shutil.rmtree(DIR_NAME)
+    with common_proc() as _:
+        path = os.path.join(DIR_NAME, _SharedDataFileNames.result.value)
+        ans = {"cumtime": [], "loss": []}
+        for i in range(19):
+            _record_result(path, results={"loss": i, "cumtime": i}, lock=LOCK)
+            ans["loss"].append(i)
+            ans["cumtime"].append(i)
+            assert not _is_simulator_terminated(path, max_evals=20, lock=LOCK)
+            assert json.load(open(path)) == ans
+        else:
+            _record_result(path, results={"loss": i + 1, "cumtime": i + 1}, lock=LOCK)
+            ans["loss"].append(i + 1)
+            ans["cumtime"].append(i + 1)
+            assert _is_simulator_terminated(path, max_evals=20, lock=LOCK)
+            assert json.load(open(path)) == ans
 
 
 if __name__ == "__main__":
