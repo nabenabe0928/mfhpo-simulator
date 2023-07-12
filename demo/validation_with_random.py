3297a0f650d6475c7f8eddd49b61532eeef3fbbb
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-07-12 20:54:07 +0200

    [refactor] [feat] Add demo for raw time measure

--- demo/validation_with_random.py
+++ demo/validation_with_random.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 import multiprocessing
+import os
 import time
 from argparse import ArgumentParser
 
@@ -17,16 +18,22 @@ import numpy as np
 parser = ArgumentParser()
 parser.add_argument("--n_evals", type=int, default=100)
 parser.add_argument("--n_workers", type=int, default=8)
-parser.add_argument("--n_seeds", type=int, default=10)
+parser.add_argument("--seed", type=int, default=10)
 parser.add_argument("--runtime_factor", type=float, default=100.0)
+parser.add_argument("--raw", type=str, choices=["True", "False"], default="False")
+parser.add_argument("--deterministic", type=str, choices=["True", "False"], default="False")
+parser.add_argument("--mode", type=str, choices=["single", "multi", "no"], default="single")
 args = parser.parse_args()
 
 FIDEL_KEY = "z0"
 OBJ_KEY = "loss"
 RUNTIME_KEY = "runtime"
+RAW = eval(args.raw)
+MODE = args.mode
+DETERMINISTIC = eval(args.deterministic)
 N_WORKERS = args.n_workers
 N_EVALS = args.n_evals
-N_SEEDS = args.n_seeds
+SEED = args.seed
 RUNTIME_FACTOR = args.runtime_factor
 
 
@@ -139,49 +146,37 @@ def run_without_wrapper(bench: MFHartmann, seed: int) -> tuple[np.ndarray, np.nd
     return extract(results, start=start)
 
 
+def get_result_without_simulator(bench, seed: int) -> tuple[list[float], list[float], None]:
+    loss_vals, actual_cumtime = run_without_wrapper(bench, seed=seed)
+    return loss_vals.tolist(), actual_cumtime.tolist(), None
+
+
+def get_result_with_single_core_simulator(bench, seed: int) -> tuple[list[float], list[float], list[float]]:
+    loss_vals, simulated_cumtime, actual_cumtime = run_with_wrapper(bench, seed=seed, ask_and_tell=True)
+    return loss_vals.tolist(), actual_cumtime.tolist(), simulated_cumtime.tolist()
+
+
+def get_result_with_multi_core_simulator(bench, seed: int) -> tuple[list[float], list[float], list[float]]:
+    loss_vals, simulated_cumtime, actual_cumtime = run_with_wrapper(bench, seed=seed)
+    return loss_vals.tolist(), actual_cumtime.tolist(), simulated_cumtime.tolist()
+
+
 def main(deterministic: bool):
-    data = {
-        "naive": {
-            "loss": [],
-            "actual_cumtime": [],
-        },
-        "ours": {
-            "loss": [],
-            "actual_cumtime": [],
-            "simulated_cumtime": [],
-        },
-        "ours_ask_and_tell": {
-            "loss": [],
-            "actual_cumtime": [],
-            "simulated_cumtime": [],
-        },
-    }
+    data = {k: dict(loss=[], actual_cumtime=[], simulated_cumtime=[]) for k in ["naive", "ours", "ours_ask_and_tell"]}
     bench = MFHartmann(dim=6, runtime_factor=RUNTIME_FACTOR, deterministic=deterministic)
-    for seed in range(N_SEEDS):
+    for seed in range(SEED):
         print(f"Run with {seed=}")
 
-        time.sleep(0.01)
-        bench.reseed(seed)
-        loss_vals, simulated_cumtime, actual_cumtime = np.array(run_with_wrapper(bench, seed=seed))
-        data["ours"]["loss"].append(loss_vals.tolist())
-        data["ours"]["actual_cumtime"].append(actual_cumtime.tolist())
-        data["ours"]["simulated_cumtime"].append(simulated_cumtime.tolist())
-
-        time.sleep(0.01)
-        bench.reseed(seed)
-        loss_vals, simulated_cumtime, actual_cumtime = np.array(run_with_wrapper(bench, seed=seed, ask_and_tell=True))
-        data["ours_ask_and_tell"]["loss"].append(loss_vals.tolist())
-        data["ours_ask_and_tell"]["actual_cumtime"].append(actual_cumtime.tolist())
-        data["ours_ask_and_tell"]["simulated_cumtime"].append(simulated_cumtime.tolist())
-
-        # bench.reseed(seed)
-        # loss_vals, actual_cumtime = np.array(run_without_wrapper(bench, seed=seed))
-        # data["naive"]["loss"].append(loss_vals.tolist())
-        # data["naive"]["actual_cumtime"].append(actual_cumtime.tolist())
-
-        # naive_loss, our_loss = data["naive"]["loss"][-1], data["ours"]["loss"][-1]
-        # percent = 100 * np.sum(np.isclose(naive_loss, our_loss)) / len(our_loss)
-        # print(f"How much was correct?: {percent:.2f}%. NOTE: Tie-break could cause False!")
+        for key, result_fn in {
+            "ours": get_result_with_multi_core_simulator,
+            "ours_ask_and_tell": get_result_with_single_core_simulator,
+            "naive": get_result_without_simulator,
+        }.items():
+            bench.reseed(seed)
+            loss_vals, simulated_cumtime, actual_cumtime = run_with_wrapper(bench, seed=seed)
+            data[key]["loss"].append(loss_vals)
+            data[key]["actual_cumtime"].append(actual_cumtime)
+            data[key]["simulated_cumtime"].append(simulated_cumtime)
 
         suffix = "deterministic" if deterministic else "noisy"
         with open(f"demo/validation-results-{suffix}.json", mode="w") as f:
@@ -189,5 +184,20 @@ def main(deterministic: bool):
 
 
 if __name__ == "__main__":
-    main(deterministic=True)
-    main(deterministic=False)
+    if not RAW:
+        main(deterministic=True)
+        main(deterministic=False)
+    else:
+        bench = MFHartmann(dim=6, runtime_factor=RUNTIME_FACTOR, deterministic=DETERMINISTIC)
+
+        if MODE == "single":
+            loss, actual_cumtime, simulated_cumtime = get_result_with_single_core_simulator(bench, SEED)
+        elif MODE == "multi":
+            loss, actual_cumtime, simulated_cumtime = get_result_with_multi_core_simulator(bench, SEED)
+        else:
+            loss, actual_cumtime = get_result_without_simulator(bench, SEED)
+
+        dir_name = f"demo/random_{MODE}_noise={not DETERMINISTIC}"
+        os.makedirs(dir_name, exist_ok=True)
+        with open(os.path.join(dir_name, f"{SEED:0>3}.json"), mode="w") as f:
+            json.dump(dict(loss=loss, actual_cumtime=actual_cumtime, simulated_cumtime=simulated_cumtime), f, indent=4)
