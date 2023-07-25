3eef47be6568ff4d6cba0cf890772522ba01c917
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-07-25 10:24:50 +0200

    Update to v1.2.6 before supporting expensive sampler

--- benchmark_simulator/__init__.py
+++ benchmark_simulator/__init__.py
@@ -2,7 +2,7 @@ from benchmark_simulator._constants import AbstractAskTellOptimizer, ObjectiveFu
 from benchmark_simulator.simulator import ObjectiveFuncWrapper, get_multiple_wrappers
 
 
-__version__ = "1.2.4"
+__version__ = "1.2.6"
 __copyright__ = "Copyright (C) 2023 Shuhei Watanabe"
 __licence__ = "Apache-2.0 License"
 __author__ = "Shuhei Watanabe"
--- setup.py
+++ setup.py
@@ -6,14 +6,20 @@ with open("requirements-for-setup.txt", "r") as f:
     for line in f:
         requirements.append(line.strip())
 
+pkgs = [
+    "benchmark_simulator",
+    "benchmark_simulator/_simulator",
+    "benchmark_simulator/utils",
+    "benchmark_simulator/_trackers",
+]
 
 setuptools.setup(
     name="mfhpo-simulator",
-    version="1.2.4",
+    version="1.2.6",
     author="nabenabe0928",
     author_email="shuhei.watanabe.utokyo@gmail.com",
     url="https://github.com/nabenabe0928/mfhpo-simulator",
-    packages=["benchmark_simulator", "benchmark_simulator/_simulator"],
+    packages=pkgs,
     python_requires=">=3.8",
     platforms=["Linux", "Darwin"],
     install_requires=requirements,
