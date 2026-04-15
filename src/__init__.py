66e8034d73c7ead7e75a5c0d729b25d29b518b22
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2026-04-15 20:34:26 +0900

    Fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from src._constants import AbstractAskTellOptimizer
 from src.simulator import ObjectiveFuncWrapper
 
 
-if TYPE_CHECKING:
-    from src._constants import ObjectiveFuncType
-
-
-__all__ = [
-    "AbstractAskTellOptimizer",
-    "ObjectiveFuncWrapper",
-    "ObjectiveFuncType",
-]
+__all__ = ["AbstractAskTellOptimizer", "ObjectiveFuncWrapper"]
